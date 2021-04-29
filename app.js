8f45ae1c66bef2217a1423dadaccea41125b8fa4
stylelinz <cwchuang1997@gmail.com>
2021-04-29 22:12:29 +0800

    feat: add delete restaurant function + app.js, index.hbs, show.hbs #20210429007

--- app.js
+++ app.js
@@ -85,6 +85,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(err => console.log(err))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const { id } = req.params
+  return Restaurants.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
 // 不要忘記開監聽
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`)
