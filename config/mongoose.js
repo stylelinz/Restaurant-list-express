4cf1841a41a9a5969580b077d0ed87eeb3ed24a3
stylelinz <cwchuang1997@gmail.com>
2021-05-23 09:41:54 +0800

    feat: add user model + /models/User.js #20210523001

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 })
 
--- models/restaurant-list.js
+++ models/restaurant-list.js
@@ -2,39 +2,19 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const restaurantSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  name_en: {
-    type: String
-  },
-  category: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  phone: {
-    type: String,
-    required: true
-  },
-  google_map: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: Number,
-    required: true
-  },
-  description: {
-    type: String,
+  name: { type: String, required: true },
+  name_en: { type: String },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  phone: { type: String, required: true },
+  google_map: { type: String, required: true },
+  rating: { type: Number, required: true },
+  description: { type: String, required: true },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
     required: true
   }
 })
