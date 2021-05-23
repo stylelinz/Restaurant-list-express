7c068b16f8317437e6b88635d2920b3ce6bf2599
stylelinz <cwchuang1997@gmail.com>
2021-05-23 16:39:32 +0800

    feat: add login authentication feature + middleware/auth.js #20210523005

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
 const User = require('../../models/User')
 
@@ -7,16 +8,28 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  console.log(req.body)
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/register', (req, res) => {
-  console.log(req.body)
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  try {
+    const user = await User.findOne({ email })
+    if (user) {
+      console.log('the user is already exist')
+      return res.render('login')
+    }
+    await User.create({ name, email, password })
+    return res.redirect('/users/login')
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.post('/logout', (req, res) => {
