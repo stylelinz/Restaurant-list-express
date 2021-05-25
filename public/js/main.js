7b46d2ea3f04b7013a536c74ccc6aa6da5052975
stylelinz <cwchuang1997@gmail.com>
2021-05-25 17:55:48 +0800

    feat: add confirm before remove restaurant from list > public/js/main.js #20210525005

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
 const panel = document.querySelector('#data-panel')
-panel.addEventListener('click', (event) => {
-  const { target } = event
-  if (target.matches('.btn-danger')) {
-    if (!window.confirm('確定要刪除餐廳嗎?')) {
-      event.preventDefault()
+if (panel) {
+  panel.addEventListener('click', (event) => {
+    const { target } = event
+    if (target.matches('.btn-danger')) {
+      if (!window.confirm('確定要刪除餐廳嗎?')) {
+        event.preventDefault()
+      }
     }
-  }
-})
+  })
+}
